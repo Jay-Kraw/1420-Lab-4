48cf11a3b2e0e33267221e9f2160bbd408efc075
Jay-Kraw <krawiecjacob@gmail.com>
2026-01-29 11:27:28 -0500

    Removed LAB4Class and reverted change of LAB4MAIN to also remove LAB4CLASS functionality

--- LAB4Main.java
+++ LAB4Main.java
@@ -1,7 +1,5 @@
 public class LAB4Main {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-        Lab4CLASS Test = new Lab4CLASS();
-        System.out.println(Test.print());
     }
 }
